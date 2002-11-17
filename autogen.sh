5bfd17bed9d74682f2e7f6727406eeaece6800bb
Jean Bréfort <jean.brefort@normalesup.org>
2002-11-17 08:35:58 +0000

    initial version

---
+++ autogen.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+## First, find where automake is installed and get the version
+AMPATH=`which automake|sed 's/\/bin\/automake//'`
+AMVER=`automake --version|grep automake|awk '{print $4}'|awk -F. '{print $1"."$2}'`
+
+## Copy files from the automake directory
+ln -sf $AMPATH/share/automake-$AMVER/missing .
+ln -sf $AMPATH/share/automake-$AMVER/mkinstalldirs .
+ln -sf $AMPATH/share/automake-$AMVER/install-sh .
+ln -sf $AMPATH/share/automake-$AMVER/depcomp .
+
+## run aclocal
+aclocal -I /usr/share/aclocal/gnome2-macros
+
+## libtool and intltool
+libtoolize
+intltoolize
+
+## autoheader, automake, autoconf
+autoheader
+automake
+autoconf
+
+## Job ended
+echo "run ./configure with the appropriate options, the make and enjoy"
