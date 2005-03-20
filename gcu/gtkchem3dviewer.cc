29f723be8ba430b56e529f161ab8d863f1bf7794
Jean Bréfort <jean.brefort@normalesup.org>
2005-03-20 17:29:09 +0000

    2005-03-20  Jean Brefort  <jean.brefort@normalesup.org> 	* gcu/gtkchem3dviewer.cc: use GNOME_VFS_FILE_INFO_FORCE_SLOW_MIME_TYPE 	when detecting mime types.

--- gcu/gtkchem3dviewer.cc
+++ gcu/gtkchem3dviewer.cc
@@ -412,7 +412,9 @@ void gtk_chem3d_viewer_set_uri (GtkChem3DViewer * viewer, gchar *uri)
 		gnome_vfs_file_info_unref (info);
 		return;
 	}
-	gnome_vfs_get_file_info_from_handle (handle, info, GNOME_VFS_FILE_INFO_GET_MIME_TYPE);
+	gnome_vfs_get_file_info_from_handle (handle, info,
+		(GnomeVFSFileInfoOptions)(GNOME_VFS_FILE_INFO_GET_MIME_TYPE |
+							GNOME_VFS_FILE_INFO_FORCE_SLOW_MIME_TYPE));
 	gchar *buf = new gchar[info->size + 1];
 	GnomeVFSFileSize n;
 	gnome_vfs_read (handle, buf, info->size, &n);
