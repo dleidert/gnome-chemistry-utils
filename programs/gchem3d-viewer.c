55a3dda6e5d8b3e195fc5928de052e0082adca32
Jean Bréfort <jean.brefort@normalesup.org>
2005-06-26 13:26:37 +0000

    2005-06-26  Jean Brefort  <jean.brefort@normalesup.org> 	* database/isotopes.xml.in: fixed typos. 	* programs/Makefile.am: added gchemcalc desktop file. 	* programs/gchem3d-viewer.c: added menus. 	* programs/gchem3d-viewer.desktop.in: added categories. 	* programs/gchem3d-viewer-unstable.desktop.in: ditto. 	* programs/gchemcalc.desktop.in: new desktop file for gchemcalc. 	* programs/gchemcalc-unstable.desktop.in: ditto.

--- programs/gchem3d-viewer.c
+++ programs/gchem3d-viewer.c
@@ -26,6 +26,7 @@
 #include <gcu/chemistry.h>
 #include <gcu/gtkchem3dviewer.h>
 #include <glib.h>
+#include <glib/gi18n.h>
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <libgnomevfs/gnome-vfs.h>
@@ -33,12 +34,54 @@
 /*!\file
 A simple sample of the use of the GtkChem3DViewer widget.
 */
+
+static void on_file_open (GtkWidget *widget, void *data)
+{
+}
+
+static void on_quit (GtkWidget *widget, void *data)
+{
+	gtk_main_quit();
+}
+
+static void on_about (GtkWidget *widget, void *data)
+{
+}
+
+static GtkActionEntry entries[] = {
+  { "FileMenu", NULL, N_("_File") },
+	  { "Open", GTK_STOCK_OPEN, N_("_Open..."), "<control>O",
+		  N_("Open a file"), G_CALLBACK (on_file_open) },
+ 	  { "Quit", GTK_STOCK_QUIT, N_("_Quit"), "<control>Q",
+		  N_("Quit GChem3D"), G_CALLBACK (on_quit) },
+  { "HelpMenu", NULL, N_("_Help") },
+	  { "About", NULL, N_("_About"), NULL,
+		  N_("About GChem3D"), G_CALLBACK (on_about) }
+};
+
+static const char *ui_description =
+"<ui>"
+"  <menubar name='MainMenu'>"
+"    <menu action='FileMenu'>"
+"      <menuitem action='Open'/>"
+"      <menuitem action='Quit'/>"
+"    </menu>"
+"    <menu action='HelpMenu'>"
+"      <menuitem action='About'/>"
+"    </menu>"
+"  </menubar>"
+"</ui>";
+
 int main(int argc, char *argv[])
 {
 	GtkWidget *window;
-	GtkWidget *viewer;
+	GtkWidget *viewer, *vbox, *bar;
 	GnomeVFSURI *uri, *auri;
 	char *path, *dir;
+	GtkUIManager* ui_manager;
+	GtkActionGroup *action_group;
+	GtkAccelGroup *accel_group;
+	GError *error;
 
 	gtk_init (&argc, &argv);
 	if (!gnome_vfs_init ()) {
@@ -50,9 +93,28 @@ int main(int argc, char *argv[])
 
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(window), "GtkChem3dViewer");
+	gtk_window_set_default_size (GTK_WINDOW(window), 200, 230);
 	g_signal_connect(GTK_OBJECT(window), "destroy",
-		 GTK_SIGNAL_FUNC(gtk_main_quit),
-		 NULL);
+		GTK_SIGNAL_FUNC(gtk_main_quit),
+		NULL);
+	vbox = gtk_vbox_new (FALSE, 0);
+	gtk_container_add (GTK_CONTAINER (window), vbox);
+	ui_manager = gtk_ui_manager_new ();
+	action_group = gtk_action_group_new ("MenuActions");
+	gtk_action_group_set_translation_domain (action_group, GETTEXT_PACKAGE);
+	gtk_action_group_add_actions (action_group, entries, G_N_ELEMENTS (entries), NULL);
+	gtk_ui_manager_insert_action_group (ui_manager, action_group, 0);
+	accel_group = gtk_ui_manager_get_accel_group (ui_manager);
+	gtk_window_add_accel_group (GTK_WINDOW (window), accel_group);
+	error = NULL;
+	if (!gtk_ui_manager_add_ui_from_string (ui_manager, ui_description, -1, &error))
+	  {
+		g_message ("building menus failed: %s", error->message);
+		g_error_free (error);
+		exit (EXIT_FAILURE);
+	  }
+	bar = gtk_ui_manager_get_widget (ui_manager, "/MainMenu");
+	gtk_box_pack_start (GTK_BOX (vbox), bar, FALSE, FALSE, 0);
 
 	if (argc >= 2) {
 		path = g_get_current_dir ();
@@ -66,11 +128,11 @@ int main(int argc, char *argv[])
 		gnome_vfs_uri_unref (auri);
 		gnome_vfs_uri_unref (uri);
 		g_free (dir);
-		gtk_container_add(GTK_CONTAINER(window), viewer);
-		gtk_widget_show_all(window);
-	
-		gtk_main();
-	}
+	} else
+		viewer = gtk_chem3d_viewer_new("");
+	gtk_container_add(GTK_CONTAINER(vbox), viewer);
+	gtk_widget_show_all(window);
+	gtk_main();
 	
 	return(0);
 }
