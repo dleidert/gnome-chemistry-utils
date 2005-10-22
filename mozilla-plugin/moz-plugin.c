df2007f0278adeb082b75dcfcc7b50d418af407b
Jean Bréfort <jean.brefort@normalesup.org>
2005-10-22 09:11:26 +0000

    2005-10-22  Jean Brefort  <jean.brefort@normalesup.org> 	* mozilla-plugin/moz-plugin.c: add some code.

--- mozilla-plugin/moz-plugin.c
+++ mozilla-plugin/moz-plugin.c
@@ -26,9 +26,118 @@
 #include "npapi.h"
 #include "npupp.h"
 
+static NPNetscapeFuncs mozilla_funcs;
+
+static NPError ChemNew (NPMIMEType mime_type, NPP instance,
+				 uint16 mode, uint16 argc, char *argn[], char *argv[],
+				 NPSavedData *saved)
+{
+	return NPERR_NO_ERROR;
+}
+
+static NPError ChemDestroy (NPP instance, NPSavedData **save)
+{
+	return NPERR_NO_ERROR;
+}
+
+static NPError ChemSetWindow (NPP instance, NPWindow *window)
+{
+	return NPERR_NO_ERROR;
+}
+
+static NPError ChemNewStream (NPP instance, NPMIMEType type, NPStream *stream,
+		      NPBool seekable, uint16 *stype)
+{
+	return NPERR_NO_ERROR;
+}
+
+static NPError ChemDestroyStream (NPP instance, NPStream *stream, NPError reason)
+{
+	return NPERR_NO_ERROR;
+}
+
+static int32 ChemWriteReady (NPP instance, NPStream *stream)
+{
+	return 0;
+}
+
+static void ChemPrint (NPP instance, NPPrint *platformPrint)
+{
+}
+
+static int32 ChemWrite (NPP instance, NPStream *stream, int32 offset,
+			  int32 len, void *buffer)
+{
+	return 0;
+}
+
+static void ChemStreamAsFile (NPP instance, NPStream *stream, const char *fname)
+{
+}
+
+NPError ChemURLNotify (NPP instance, const char* url, const char* window, void* notifyData)
+{
+	return NPERR_NO_ERROR;
+}
+
+NPError NP_GetValue (void *future, NPPVariable variable, void *value)
+{
+	switch (variable) {
+	case NPPVpluginNameString:
+		*((char **) value) = "Gnome Chemistry Utils";
+		break;
+	case NPPVpluginDescriptionString:
+		*((char **) value) =
+			"Gnome Chemistry Utils "VERSION". "
+			"Chemical structures display.";
+		break;
+	default:
+		return NPERR_GENERIC_ERROR;
+	}
+	return NPERR_NO_ERROR;
+}
+
+char *NP_GetMIMEDescription (void)
+{
+	return ("chemical/x-xyz:XYZ Coordinate Format");
+}
+
 /* This is called to initialise the plugin
  */
-NPError NPP_Initialize() {
+NPError NP_Initialize(NPNetscapeFuncs *mozFuncs, NPPluginFuncs *pluginFuncs) {
+	if (mozFuncs == NULL || pluginFuncs == NULL)
+		return NPERR_INVALID_FUNCTABLE_ERROR;
+	
+	if ((mozFuncs->version >> 8) > NP_VERSION_MAJOR)
+		return NPERR_INCOMPATIBLE_VERSION_ERROR;
+	if (mozFuncs->size < sizeof (NPNetscapeFuncs))
+		return NPERR_INVALID_FUNCTABLE_ERROR;
+	if (pluginFuncs->size < sizeof (NPPluginFuncs))
+		return NPERR_INVALID_FUNCTABLE_ERROR;
 
+	memcpy (&mozilla_funcs, mozFuncs, sizeof (NPNetscapeFuncs));
+	
+	pluginFuncs->version    = (NP_VERSION_MAJOR << 8) + NP_VERSION_MINOR;
+	pluginFuncs->size       = sizeof (NPPluginFuncs);
+	pluginFuncs->newp       = NewNPP_NewProc (ChemNew);
+	pluginFuncs->destroy    = NewNPP_DestroyProc (ChemDestroy);
+	pluginFuncs->setwindow  = NewNPP_SetWindowProc (ChemSetWindow);
+	pluginFuncs->newstream  = NewNPP_NewStreamProc (ChemNewStream);
+	pluginFuncs->destroystream = NewNPP_DestroyStreamProc (ChemDestroyStream);
+	pluginFuncs->asfile     = NewNPP_StreamAsFileProc (ChemStreamAsFile);
+	pluginFuncs->writeready = NewNPP_WriteReadyProc (ChemWriteReady);
+	pluginFuncs->write      = NewNPP_WriteProc (ChemWrite);
+	pluginFuncs->print      = NewNPP_PrintProc (ChemPrint);
+	pluginFuncs->urlnotify  = NewNPP_URLNotifyProc (ChemURLNotify);
+	pluginFuncs->event      = NULL;
+#ifdef OJI
+	pluginFuncs->javaClass  = NULL;
+#endif
+
+	return NPERR_NO_ERROR;
+}
+
+NPError	NP_Shutdown(void)
+{
 	return NPERR_NO_ERROR;
 }
