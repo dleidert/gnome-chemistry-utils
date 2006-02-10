e17b3aa928eea032c339d2765e45334e670f927b
Jean Bréfort <jean.brefort@normalesup.org>
2006-02-10 07:22:02 +0000

    2006-02-10  Jean Brefort  <jean.brefort@normalesup.org> 	* tests/testgtkchem3dviewer.c: add a copyright. 	* tests/testgtkcrystalviewer.c: ditto. 	* tests/testgtkperiodic.c: ditto.

--- tests/testgtkperiodic.c
+++ tests/testgtkperiodic.c
@@ -1,3 +1,25 @@
+/* 
+ * Gnome Chemisty Utils
+ * testgtkperiodic.c 
+ *
+ * Copyright (C) 2006 Jean Bréfort <jean.brefort@normalesup.org>
+ *
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
 #include <gcu/gtkperiodic.h>
 #include <gcu/chemistry.h>
 #include <glib.h>
