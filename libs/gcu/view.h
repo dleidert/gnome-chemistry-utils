d3dce7d2762725981c53d06b2e945ff839ae4b26
Jean Bréfort <jean.brefort@normalesup.org>
2007-03-09 06:26:30 +0000

    2007-03-09  Jean Brefort  <jean.brefort@normalesup.org> 	* gcu/*: moved to libs/gcu. 	* libs/gcu/*: moved from gcu.

---
+++ libs/gcu/view.h
@@ -0,0 +1,28 @@
+// -*- C++ -*-
+
+/* 
+ * Gnome Chemistry Utils
+ * gcu/view.h
+ *
+ * Copyright (C) 2006 Jean Bréfort <jean.brefort@normalesup.org>
+ *
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+#ifndef GCU_VIEW_H
+#define GCU_VIEW_H
+
+#endif	//	GCU_VIEW_H
