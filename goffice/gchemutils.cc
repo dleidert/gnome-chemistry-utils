8012dbb6782b79e907dd850ea6f8b6561edb38b1
Jean Bréfort <jean.brefort@normalesup.org>
2007-04-13 06:12:50 +0000

    2007-04-13  Jean Brefort  <jean.brefort@normalesup.org> 	* Makefile.am: add new directories. 	* configure.ac: ditto. 	* goffice/Makefile.am: initial work on the goffice plugin. 	* goffice/gchemutils-priv.h: ditto. 	* goffice/gchemutils.cc: ditto. 	* goffice/gchemutils.h: ditto. 	* goffice/plugin.xml.in: ditto. 	* programs/paint/Makefile.am: enhanced build.

---
+++ goffice/gchemutils.cc
@@ -0,0 +1,131 @@
+/* 
+ * GChemUtils GOffice component
+ * gchemutils.cc
+ *
+ * Copyright (C) 2005-2007 Jean Bréfort <jean.brefort@normalesup.org>
+ *
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+#include "config.h"
+#include "gchemutils-priv.h"
+
+#include <gsf/gsf-impl-utils.h>
+#include <goffice/app/module-plugin-defs.h>
+#include <goffice/component/go-component-factory.h>
+#include <openbabel/mol.h>
+
+//gcuGOfficeApplication *app;
+
+extern "C"
+{
+
+extern GOPluginModuleDepend const go_plugin_depends [] = {
+    { "goffice", GOFFICE_API_VERSION }
+};
+extern GOPluginModuleHeader const go_plugin_header =
+	{ GOFFICE_MODULE_PLUGIN_MAGIC_NUMBER, G_N_ELEMENTS (go_plugin_depends) };
+
+static GObjectClass *gogcu_parent_klass;
+
+static gboolean
+go_gchemutils_component_get_data (GOComponent *component, gpointer *data, int *length,
+									void (**clearfunc) (gpointer), gpointer *user_data)
+{
+	GOGChemUtilsComponent *gogcu = GO_GCHEMUTILS_COMPONENT (component);
+	bool result = false;
+	return result;
+}
+
+static void
+go_gchemutils_component_set_data (GOComponent *component)
+{
+	GOGChemUtilsComponent *gogcu = GO_GCHEMUTILS_COMPONENT (component);
+}
+	
+static void
+go_gchemutils_component_draw_cairo (GOComponent *component, gpointer data,
+						  double width, double height)
+{
+	GOGChemUtilsComponent *gogcu = GO_GCHEMUTILS_COMPONENT (component);		  
+}
+
+static gboolean
+go_gchemutils_component_edit (GOComponent *component)
+{
+	GOGChemUtilsComponent *gogcu = GO_GCHEMUTILS_COMPONENT (component);
+	return false;
+}
+
+static void
+go_gchemutils_component_finalize (GObject *obj)
+{
+	GOGChemUtilsComponent *gogcu = GO_GCHEMUTILS_COMPONENT (obj);
+	G_OBJECT_CLASS (gogcu_parent_klass)->finalize (obj);
+}
+
+static void
+go_gchemutils_component_init (GOComponent *component)
+{
+	component->resizable = false;
+	component->editable = false;	// set it to true for editable docs
+	component->window = NULL;
+	component->ascent = 1.;
+	component->descent = 0.;
+	component->width = 1.;
+}
+
+static void
+go_gchemutils_component_class_init (GOComponentClass *klass)
+{
+	GObjectClass *obj_klass = (GObjectClass *) klass;
+	obj_klass->finalize = go_gchemutils_component_finalize;
+
+	gogcu_parent_klass = (GObjectClass*) g_type_class_peek_parent (klass);
+
+	klass->get_data = go_gchemutils_component_get_data;
+	klass->set_data = go_gchemutils_component_set_data;
+	klass->draw_cairo = go_gchemutils_component_draw_cairo;
+	klass->edit = go_gchemutils_component_edit;
+}
+
+GSF_DYNAMIC_CLASS (GOGChemUtilsComponent, go_gchemutils_component,
+	go_gchemutils_component_class_init, go_gchemutils_component_init,
+	GO_COMPONENT_TYPE)
+
+/*************************************************************************************/
+
+G_MODULE_EXPORT void
+go_plugin_init (GOPlugin *plugin, GOCmdContext *cc)
+{
+	bindtextdomain (GETTEXT_PACKAGE, DATADIR"/locale");
+#ifdef ENABLE_NLS
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+#endif
+	GTypeModule *module = go_plugin_get_type_module (plugin);
+	go_gchemutils_component_register_type (module);
+	go_components_set_mime_suffix ("chemical/x-xyz", "*.xyz");
+	go_components_set_mime_suffix ("application/x-gchempaint", "*.gchempaint");
+	go_components_set_mime_suffix ("application/x-gcrystal", "*.gcrystal");
+// TODO: add other types
+}
+
+G_MODULE_EXPORT void
+go_plugin_shutdown (GOPlugin *plugin, GOCmdContext *cc)
+{
+}
+
+}	// extern "C"
---
+++ goffice/gchemutils.h
@@ -0,0 +1,43 @@
+/* 
+ * GChemUtils GOffice component
+ * gchemutils.h 
+ *
+ * Copyright (C) 2005-2007 Jean Bréfort <jean.brefort@normalesup.org>
+ *
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+#ifndef GOFFICE_GCHEMPAINT_H
+#define GOFFICE_GCHEMPAINT_H
+
+#include <goffice/component/goffice-component.h>
+#include <goffice/component/go-component.h>
+
+extern "C"
+{
+
+typedef struct _GOGChemUtilsComponent GOGChemUtilsComponent;
+
+typedef GOComponentClass GOGChemUtilsComponentClass;
+
+#define GO_GCHEMUTILS_COMPONENT_TYPE	(go_gchemutils_component_get_type ())
+#define GO_GCHEMUTILS_COMPONENT(o)		(G_TYPE_CHECK_INSTANCE_CAST ((o), GO_GCHEMUTILS_COMPONENT_TYPE, GOGChemUtilsComponent))
+#define GO_IS_GCHEMUTILS_COMPONENT(o)	(G_TYPE_CHECK_INSTANCE_TYPE ((o), GO_GCHEMUTILS_COMPONENT_TYPE))
+
+GType go_gchemutils_component_get_type (void);
+
+}
+#endif	// GOFFICE_GCHEMPAINT_H
