bb7a21e87df97935d14c3bb1afc8c075cb707779
Jean Bréfort <jean.brefort@normalesup.org>
2007-12-21 07:30:09 +0000

    2007-12-21  Jean Brefort  <jean.brefort@normalesup.org> 	* configure.ac: post release version bump (0.9.3). 	* plugins/loaders/Makefile.am: add new cds directory. 	* plugins/loaders/cdx/Makefile.am: new plugin. 	* plugins/loaders/cdx/cdx.cc: ditto. 	* plugins/loaders/cdx/plugin.xml.in: ditto.

---
+++ plugins/loaders/cdx/cdx.cc
@@ -0,0 +1,90 @@
+// -*- C++ -*-
+
+/* 
+ * CDXML files loader plugin
+ * cdx.cc 
+ *
+ * Copyright (C) 2007 Jean Bréfort <jean.brefort@normalesup.org>
+ *
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+#include "config.h"
+#include <gcu/document.h>
+#include <gcu/loader.h>
+
+#include <goffice/app/module-plugin-defs.h>
+#include <libintl.h>
+#include <cstdio>
+
+using namespace std;
+using namespace gcu;
+
+class CDXLoader: public gcu::Loader
+{
+public:
+	CDXLoader ();
+	virtual ~CDXLoader ();
+
+	bool Read (Document *doc, string &uri, char const *mime_type);
+	bool Write (Document *doc, string &uri, char const *mime_type);
+};
+
+CDXLoader::CDXLoader ()
+{
+	AddMimeType ("chemical/x-cdx");
+}
+
+CDXLoader::~CDXLoader ()
+{
+	RemoveMimeType ("chemical/x-cdx");
+}
+
+bool CDXLoader::Read  (Document *doc, string &uri, char const *mime_type)
+{
+	return true;
+}
+
+bool CDXLoader::Write  (Document *doc, string &uri, char const *mime_type)
+{
+	return true;
+}
+
+static CDXLoader loader;
+
+extern "C" {
+
+extern GOPluginModuleDepend const go_plugin_depends [] = {
+    { "goffice", GOFFICE_API_VERSION }
+};
+extern GOPluginModuleHeader const go_plugin_header =
+	{ GOFFICE_MODULE_PLUGIN_MAGIC_NUMBER, G_N_ELEMENTS (go_plugin_depends) };
+
+G_MODULE_EXPORT void
+go_plugin_init (GOPlugin *plugin, GOCmdContext *cc)
+{
+	bindtextdomain (GETTEXT_PACKAGE, DATADIR"/locale");
+#ifdef ENABLE_NLS
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+#endif
+}
+
+G_MODULE_EXPORT void
+go_plugin_shutdown (GOPlugin *plugin, GOCmdContext *cc)
+{
+}
+
+}
