f9e2d0e0a15c89c8e6d1c61f4137fd19fce65189
Jean Bréfort <jean.brefort@normalesup.org>
2009-01-11 14:18:31 +0000

    Added missing copyright and licenses

--- tests/testgtkcrystalviewer.c
+++ tests/testgtkcrystalviewer.c
@@ -1,3 +1,25 @@
+/* 
+ * Gnome Chemisty Utils
+ * tests/testgtkcrystalviewer.c 
+ *
+ * Copyright (C) 2008 Jean Bréfort <jean.brefort@normalesup.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the 
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ */
+
 #include <gcu/gtkcrystalviewer.h>
 #include <gcu/chemistry.h>
 #include <glib.h>
