b7b289fa46c5874efefc27d83b72fff3609646fe
Jean Bréfort <jean.brefort@normalesup.org>
2011-05-11 10:49:35 +0000

    2011-05-11  Jean Brefort  <jean.brefort@normalesup.org> 	* libs/gcugtk/gcuperiodic.c: make it work again. 	* programs/3d/Makefile.am: make things link again. 	* ui/libgcu/gcuperiodic.ui: fixed labels positions.

--- libs/gcugtk/gcuperiodic.c
+++ libs/gcugtk/gcuperiodic.c
@@ -27,6 +27,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <gsf/gsf-impl-utils.h>
 #include <glib/gi18n-lib.h>
 
 struct _GcuPeriodic
@@ -88,125 +89,6 @@ enum {
 
 static guint gcu_periodic_signals[LAST_SIGNAL] = { 0 };
 
-static void gcu_periodic_class_init (GcuPeriodicClass  *klass);
-static void gcu_periodic_init(GcuPeriodic *periodic);
-static void gcu_periodic_finalize(GObject *object);
-static void gcu_periodic_set_property (GObject              *object,
-					    guint                 param_id,
-					    const GValue         *value,
-					    GParamSpec           *pspec);
-static void gcu_periodic_get_property (GObject              *object,
-					    guint                 param_id,
-					    GValue               *value,
-					    GParamSpec           *pspec);
-static void on_clicked(GtkToggleButton *button, GcuPeriodic* periodic);
-
-GType
-gcu_periodic_get_type (void)
-{
-	static GType periodic_type = 0;
-  
-	if (!periodic_type)
-	{
-		static const GTypeInfo periodic_info =
-		{
-			sizeof (GcuPeriodicClass),
-			NULL,           /* base_init */
-			NULL,           /* base_finalize */
-			(GClassInitFunc) gcu_periodic_class_init,
-			NULL,           /* class_finalize */
-			NULL,           /* class_data */
-			sizeof (GcuPeriodic),
-			0,              /* n_preallocs */
-			(GInstanceInitFunc) gcu_periodic_init,
-			NULL
-		};
-
-		periodic_type = g_type_register_static (GTK_TYPE_BIN, "GcuPeriodic", &periodic_info, 0);
-	}
-  
-	return periodic_type;
-}
-
-void gcu_periodic_class_init (GcuPeriodicClass *klass)
-{
-	GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
-	parent_class = (GtkBinClass*) g_type_class_peek_parent (klass);
-	
-	gobject_class->set_property = gcu_periodic_set_property;
-	gobject_class->get_property = gcu_periodic_get_property;
-	klass->element_changed_event = NULL;
-	gcu_periodic_signals[ELEMENT_CHANGED] =
-	g_signal_new ("element_changed",
-				  G_TYPE_FROM_CLASS(gobject_class),
-				  G_SIGNAL_RUN_LAST,
-				  G_STRUCT_OFFSET(GcuPeriodicClass, element_changed_event),
-				  NULL, NULL,
-				  g_cclosure_marshal_VOID__UINT,
-				  G_TYPE_NONE, 1,
-				  G_TYPE_UINT
-				  );
-	g_object_class_install_property
-			(gobject_class,
-			 PROP_CAN_UNSELECT,
-			 g_param_spec_boolean ("can_unselect", NULL, NULL,
-				   FALSE,
-				   (G_PARAM_READABLE | G_PARAM_WRITABLE)));
-	g_object_class_install_property
-			(gobject_class,
-			 PROP_COLOR_STYLE,
-			 g_param_spec_uint ("color-style", NULL, NULL,
-								GCU_PERIODIC_COLOR_NONE, G_MAXUINT,
-								GCU_PERIODIC_COLOR_NONE,
-								(G_PARAM_READABLE | G_PARAM_WRITABLE)));
-	gobject_class->finalize = gcu_periodic_finalize;
-}
-
-void gcu_periodic_init (GcuPeriodic *periodic)
-{
-	GtkBuilder* xml;
-	char name[8] = "elt";
-	GtkToggleButton* button;
-	int i;
-	xml = go_gtk_builder_new (UIDIR"/gcuperiodic.ui", GETTEXT_PACKAGE, NULL);
-	g_return_if_fail (xml);
-	periodic->grid = GTK_GRID (gtk_builder_get_object (xml, "periodic-grid"));
-	periodic->book = GTK_NOTEBOOK (gtk_builder_get_object (xml, "book"));
-	periodic->colorstyle = GCU_PERIODIC_COLOR_NONE;
-	memset(periodic->buttons, 0, sizeof (GtkToggleButton*) * 119);
-	for (i = 1; i <= 118; i++) {
-		sprintf(name + 3, "%d", i);
-		button = (GtkToggleButton*) gtk_builder_get_object (xml, name);
-		if (GTK_IS_TOGGLE_BUTTON (button)) {
-			gtk_widget_set_tooltip_text (GTK_WIDGET(button), gcu_element_get_name(i));
-			periodic->buttons[i] = button;
-			periodic->labels[i] = GTK_LABEL (gtk_bin_get_child (GTK_BIN (button)));
-			g_signal_connect (G_OBJECT (button), "toggled", G_CALLBACK (on_clicked), periodic);
-		}
-	}
-	periodic->Z = 0;
-	gtk_container_add (GTK_CONTAINER (periodic), GTK_WIDGET (periodic->grid));
-	gtk_widget_show_all (GTK_WIDGET (periodic));
-	periodic->colorschemes = g_array_new (FALSE, FALSE, sizeof (struct ColorScheme));
-	g_object_unref (xml);
-}
-
-static void gcu_periodic_finalize (GObject *object)
-{
-	GcuPeriodic *periodic = (GcuPeriodic*) object;
-
-	g_array_free (periodic->colorschemes, FALSE);
-
-	if (G_OBJECT_CLASS (parent_class)->finalize)
-		(* G_OBJECT_CLASS (parent_class)->finalize) (object);
-}
-
-GtkWidget* gcu_periodic_new()
-{
-	GtkBin* bin = GTK_BIN(g_object_new(GCU_TYPE_PERIODIC, NULL));
-	return GTK_WIDGET(bin);
-}
-
 void on_clicked (GtkToggleButton *button, GcuPeriodic* periodic)
 {
 	static gboolean change = FALSE;
@@ -227,29 +109,6 @@ void on_clicked (GtkToggleButton *button, GcuPeriodic* periodic)
 			g_signal_emit (periodic, gcu_periodic_signals[ELEMENT_CHANGED], 0, 0);
 		} else if (periodic->buttons[0])
 			gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (periodic->buttons[0]), TRUE);
-	}
-}
-
-guint gcu_periodic_get_element(GcuPeriodic* periodic)
-{
-	g_return_val_if_fail(GCU_IS_PERIODIC(periodic), 0);
-	return periodic->Z;
-}
-
-void gcu_periodic_set_element (GcuPeriodic* periodic, guint element)
-{
-	g_return_if_fail(GCU_IS_PERIODIC(periodic));
-	if (periodic->can_unselect && periodic->buttons[0]) gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(periodic->buttons[0]), FALSE);
-	if (element) 
-	{
-		gtk_toggle_button_set_active(periodic->buttons[element], TRUE);
-		periodic->buttons[0] = periodic->buttons[element];
-		periodic->Z = element;
-	}
-	else if (periodic->can_unselect)
-	{
-		periodic->buttons[0] = NULL;
-		periodic->Z = 0;
 	}
 }
 
@@ -314,6 +173,150 @@ gcu_periodic_get_property (GObject              *object,
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, param_id, pspec);
 		break;
+	}
+}
+
+static void gcu_periodic_get_preferred_height (GtkWidget *w, gint *minimum_height, gint *natural_height)
+{
+	GtkWidget *child = gtk_bin_get_child (GTK_BIN (w));
+	gboolean visible = FALSE;
+	if (child)
+		g_object_get (G_OBJECT (child), "visible", &visible, NULL);
+	if (visible)
+		gtk_widget_get_preferred_height (child, minimum_height, natural_height);
+	else
+		*minimum_height = *natural_height = 0;
+}
+
+static void gcu_periodic_get_preferred_width (GtkWidget *w, gint *minimum_width, gint *natural_width)
+{
+	GtkWidget *child = gtk_bin_get_child (GTK_BIN (w));
+	gboolean visible = FALSE;
+	if (child)
+		g_object_get (G_OBJECT (child), "visible", &visible, NULL);
+	if (visible)
+		gtk_widget_get_preferred_width (child, minimum_width, natural_width);
+	else
+		*minimum_width = *natural_width = 0;
+}
+
+static void gcu_periodic_size_allocate (GtkWidget *w, GtkAllocation *alloc)
+{
+	GtkWidget *child = gtk_bin_get_child (GTK_BIN (w));
+	gboolean visible = FALSE;
+	if (child)
+		g_object_get (G_OBJECT (child), "visible", &visible, NULL);
+	if (visible)
+		gtk_widget_size_allocate (child, alloc);
+	(GTK_WIDGET_CLASS (parent_class))->size_allocate (w, alloc);
+}
+
+static void gcu_periodic_finalize (GObject *object)
+{
+	GcuPeriodic *periodic = (GcuPeriodic*) object;
+
+	g_array_free (periodic->colorschemes, FALSE);
+
+	if (G_OBJECT_CLASS (parent_class)->finalize)
+		(* G_OBJECT_CLASS (parent_class)->finalize) (object);
+}
+
+static void gcu_periodic_class_init (GcuPeriodicClass *klass)
+{
+	GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+	GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+	parent_class = (GtkBinClass*) g_type_class_peek_parent (klass);
+	
+	gobject_class->set_property = gcu_periodic_set_property;
+	gobject_class->get_property = gcu_periodic_get_property;
+	klass->element_changed_event = NULL;
+	gcu_periodic_signals[ELEMENT_CHANGED] =
+	g_signal_new ("element_changed",
+				  G_TYPE_FROM_CLASS(gobject_class),
+				  G_SIGNAL_RUN_LAST,
+				  G_STRUCT_OFFSET(GcuPeriodicClass, element_changed_event),
+				  NULL, NULL,
+				  g_cclosure_marshal_VOID__UINT,
+				  G_TYPE_NONE, 1,
+				  G_TYPE_UINT
+				  );
+	g_object_class_install_property
+			(gobject_class,
+			 PROP_CAN_UNSELECT,
+			 g_param_spec_boolean ("can_unselect", NULL, NULL,
+				   FALSE,
+				   (G_PARAM_READABLE | G_PARAM_WRITABLE)));
+	g_object_class_install_property
+			(gobject_class,
+			 PROP_COLOR_STYLE,
+			 g_param_spec_uint ("color-style", NULL, NULL,
+								GCU_PERIODIC_COLOR_NONE, G_MAXUINT,
+								GCU_PERIODIC_COLOR_NONE,
+								(G_PARAM_READABLE | G_PARAM_WRITABLE)));
+	gobject_class->finalize = gcu_periodic_finalize;
+	widget_class->get_preferred_height = gcu_periodic_get_preferred_height;
+	widget_class->get_preferred_width = gcu_periodic_get_preferred_width;
+	widget_class->size_allocate = gcu_periodic_size_allocate;
+}
+
+static void gcu_periodic_init (GcuPeriodic *periodic)
+{
+	GtkBuilder* xml;
+	char name[8] = "elt";
+	GtkToggleButton* button;
+	int i;
+	xml = go_gtk_builder_new (UIDIR"/gcuperiodic.ui", GETTEXT_PACKAGE, NULL);
+	g_return_if_fail (xml);
+	periodic->grid = GTK_GRID (gtk_builder_get_object (xml, "periodic-grid"));
+	periodic->book = GTK_NOTEBOOK (gtk_builder_get_object (xml, "book"));
+	periodic->colorstyle = GCU_PERIODIC_COLOR_NONE;
+	memset(periodic->buttons, 0, sizeof (GtkToggleButton*) * 119);
+	for (i = 1; i <= 118; i++) {
+		sprintf(name + 3, "%d", i);
+		button = (GtkToggleButton*) gtk_builder_get_object (xml, name);
+		if (GTK_IS_TOGGLE_BUTTON (button)) {
+			gtk_widget_set_tooltip_text (GTK_WIDGET(button), gcu_element_get_name(i));
+			periodic->buttons[i] = button;
+			periodic->labels[i] = GTK_LABEL (gtk_bin_get_child (GTK_BIN (button)));
+			g_signal_connect (G_OBJECT (button), "toggled", G_CALLBACK (on_clicked), periodic);
+		}
+	}
+	periodic->Z = 0;
+	gtk_container_add (GTK_CONTAINER (periodic), GTK_WIDGET (periodic->grid));
+	gtk_widget_show_all (GTK_WIDGET (periodic));
+	periodic->colorschemes = g_array_new (FALSE, FALSE, sizeof (struct ColorScheme));
+	g_object_unref (xml);
+}
+
+GSF_CLASS (GcuPeriodic, gcu_periodic,
+           gcu_periodic_class_init, gcu_periodic_init,
+           GTK_TYPE_BIN)
+
+GtkWidget* gcu_periodic_new ()
+{
+	return GTK_WIDGET (g_object_new (GCU_TYPE_PERIODIC, NULL));
+}
+
+guint gcu_periodic_get_element(GcuPeriodic* periodic)
+{
+	g_return_val_if_fail(GCU_IS_PERIODIC(periodic), 0);
+	return periodic->Z;
+}
+
+void gcu_periodic_set_element (GcuPeriodic* periodic, guint element)
+{
+	g_return_if_fail(GCU_IS_PERIODIC(periodic));
+	if (periodic->can_unselect && periodic->buttons[0]) gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(periodic->buttons[0]), FALSE);
+	if (element) 
+	{
+		gtk_toggle_button_set_active(periodic->buttons[element], TRUE);
+		periodic->buttons[0] = periodic->buttons[element];
+		periodic->Z = element;
+	}
+	else if (periodic->can_unselect)
+	{
+		periodic->buttons[0] = NULL;
+		periodic->Z = 0;
 	}
 }
 
