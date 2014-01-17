81de7b298e13c9b8f7ffe4b3e1de982b52d983e4
Jean Bréfort <jean.brefort@normalesup.org>
2014-01-17 07:18:00 +0000

    2014-01-17  Koop Mast 	* libs/gcu/application.cc: fix build on FreeBSD. [#41256] 	* libs/gcugtk/spectrumdoc.cc: ditto.

--- libs/gcugtk/spectrumdoc.cc
+++ libs/gcugtk/spectrumdoc.cc
@@ -1478,7 +1478,7 @@ void SpectrumDocument::ReadDataTable (istream &s, double *x, double *y)
 			break;	// this should not occur, but a corrupted or bad file is always possible
 		s.getline (line, 300);
 		if (strstr (line, "##")) {
-			s.seekg (-strlen (line) -1, _S_cur);
+			s.seekg (-strlen (line) -1, s.cur);
 			if (read > npoints) {
 				g_warning (_("Found too many data!"));
 				// FIXME: throw an exception
