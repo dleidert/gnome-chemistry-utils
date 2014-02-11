0aa41d28a96d87f88c84e3e252f08695d3107bc6
Jean Bréfort <jean.brefort@normalesup.org>
2014-02-11 21:08:22 +0000

    Add missing files.

---
+++ libs/gcp/atom-residue.cc
@@ -0,0 +1,115 @@
+// -*- C++ -*-
+
+/*
+ * GChemPaint library
+ * atom-residue.cc
+ *
+ * Copyright (C) 2014 Jean Bréfort <jean.brefort@normalesup.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+#include "config.h"
+#include "atom-residue.h"
+#include "document.h"
+#include <cstring>
+
+using namespace gcu;
+
+namespace gcp {
+
+gcu::TypeId ResidueType = gcu::NoType;
+
+AtomResidue::AtomResidue (): Atom (), m_Residue (NULL)
+{
+	SetZ (-1);
+}
+
+AtomResidue::~AtomResidue ()
+{
+	if (m_Residue)
+		const_cast <Residue *> (m_Residue)->Unref ();
+}
+
+void AtomResidue::SetResidue (Residue const *res, char const *symbol)
+{
+	if (m_Residue)
+		const_cast <Residue *> (m_Residue)->Unref ();
+	if (symbol)
+		m_Abbrev = symbol;
+	else
+		m_Abbrev = (*res->GetSymbols ().begin ()).first;
+	m_Residue = res;
+	const_cast <Residue *> (m_Residue)->Ref ();
+}
+
+const gchar* AtomResidue::GetSymbol () const
+{
+	return m_Abbrev.c_str ();
+}
+
+xmlNodePtr AtomResidue::Save (xmlDocPtr xml) const
+{
+	xmlNodePtr node = Atom::Save (xml);
+	if (node) {
+		xmlNodeSetName (node, (xmlChar const *) "residue");
+		static_cast <Document *> (GetDocument ())->SaveResidue (m_Residue, node);
+	}
+	return node;
+}
+
+bool AtomResidue::Load (xmlNodePtr node)
+{
+	bool result = Atom::Load (node);
+	if (!result)
+		return false;
+	m_Z = -1; // Atom::Load will have set it to 0.
+	xmlNodePtr child = node->children;
+	if (!child)
+		return false;
+	char *buf = NULL;
+	if (!strcmp ((const char*) child->name, "text"))
+		buf = (char*) xmlNodeGetContent (child);
+	if (!buf || !strlen (buf))
+		return false;
+	Residue *residue = (Residue*) Residue::GetResidue (buf, NULL);
+	Document *doc = static_cast <Document *> (GetDocument ());
+	gcu::Application *app = doc->GetApp ();
+	if (residue) {
+		if (child->next) {
+			Residue *res0 = new Residue (NULL, NULL, NULL, doc);
+			res0->Load (node, false, app);
+			if (*residue == *(res0->GetMolecule ()))
+				delete res0; // OK, same molecule
+			else {
+				// TODO: append residue to the document
+			}
+		}
+	} else if (child->next) {
+		residue = new Residue ();
+		residue->Load (node, false, app);
+		residue->Register ();
+		// TODO: append residue to the local residues ( or to the document?)
+	} else
+		return false;
+	m_Abbrev = buf;
+	m_Residue = residue;
+	const_cast <Residue *> (m_Residue)->Ref ();
+	xmlFree ((xmlChar*) buf);
+	return true;
+}
+
+}	//	namespace gcp
---
+++ libs/gcp/atom-residue.h
@@ -0,0 +1,87 @@
+// -*- C++ -*-
+
+/*
+ * GChemPaint library
+ * atom-residue.h
+ *
+ * Copyright (C) 2008-2014 Jean Bréfort <jean.brefort@normalesup.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+#ifndef GCHEMPAINT_ATOM_RESIDUE_H
+#define GCHEMPAINT_ATOM_RESIDUE_H
+
+#include "atom.h"
+#include "residue.h"
+#include <string>
+
+/*!\file*/
+namespace gcp {
+
+extern gcu::TypeId ResidueType;
+
+/*!\class FragmentResidue gcp/fragment-residue.h
+Represents a residue inside a atoms group (see gcp::Fragment class).
+*/
+class AtomResidue: public Atom
+{
+public:
+/*!
+The default constructor.
+*/
+	AtomResidue ();
+	virtual ~AtomResidue ();
+
+/*!
+@param xml 	the xmlDoc used to save the document.
+
+Builds an XML node representing this instance.
+@return the new XML node or NULL on error.
+*/
+	xmlNodePtr Save (xmlDocPtr xml) const;
+/*!
+@param node an XML node.
+
+Loads the symbol and associated residue from \a node.
+@return true on success, false otherwise.
+*/
+	bool Load (xmlNodePtr node);
+/*!
+@param res the residue corresponding to the symbol.
+@param symbol the symbol to use, or NULL to use the default symbol.
+
+Sets the associated residue.
+*/
+	void SetResidue (Residue const *res, char const *symbol = NULL);
+/*!
+@return the symbol of this Residue.
+*/
+	const gchar* GetSymbol () const;
+
+/*!\fn GetResidue()
+@return the associated residue.
+*/
+GCU_RO_PROP (Residue const *, Residue)
+/*!\fn GetAbbrev()
+@return the used symbol for the residue.
+*/
+GCU_RO_PROP (std::string, Abbrev)
+};
+
+}	//	namespace gcp
+
+#endif	//	GCHEMPAINT_ATOM_RESIDUE_H
